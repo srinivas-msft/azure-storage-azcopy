a88ecaa1db3843707da0bd2b168ba7cf1247073a
Narasimha Kulkarni <63087328+nakulkar-msft@users.noreply.github.com>
2023-04-18 15:21:47 +0530

    Inject Error Policy3

--- ste/injectErrorPolicy.go
+++ ste/injectErrorPolicy.go
@@ -2,12 +2,22 @@ package ste
 
 import (
 	"context"
+	"time"
 
 	"github.com/Azure/azure-pipeline-go/pipeline"
 )
 
 // NewRequestLogPolicyFactory creates a RequestLogPolicyFactory object configured using the specified options.
 func NewInjectErrorPolicyFactory() pipeline.Factory {
+	blocked := true
+	go func() {
+		for {
+			<-time.After(30 * time.Second)
+			blocked = !blocked
+			<-time.After(2 * time.Minute)
+			blocked = !blocked
+		}
+	}()
 	return pipeline.FactoryFunc(func(next pipeline.Policy, po *pipeline.PolicyOptions) pipeline.PolicyFunc {
 		// These variables are per-policy; shared by multiple calls to Do
 		var try int32
@@ -16,7 +26,7 @@ func NewInjectErrorPolicyFactory() pipeline.Factory {
 
 			response, err = next.Do(ctx, request) // Make the request\
 
-			if try == 1 {
+			if blocked && try == 1 && request.Method == "PUT" {
 				response.Response().StatusCode = 500
 				response.Response().Status = "500 Operation could not be completed within the specified time."
 			}
